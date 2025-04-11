7b7078e90920e93f099b56953fbf68802fadb827
Geoff <geoff.o@ecobee.com>
2025-04-11 10:47:25 -0400

    feat: ✨ avente disabled for markdown
    I found it was getting in my way; at least for now.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -196,14 +196,14 @@ require('lazy').setup({
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      -- {
+      --   -- Make sure to set this up properly if you have lazy=true
+      --   'MeanderingProgrammer/render-markdown.nvim',
+      --   opts = {
+      --     file_types = { "markdown", "Avante" },
+      --   },
+      --   ft = { "markdown", "Avante" },
+      -- },
     },
   },
 
